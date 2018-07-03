a0dae08bb0e097acf7de57134dd07b3c2d60aaed
rickles42 <81503+rickles42@users.noreply.github.com>
2018-07-03 09:36:48 -0700

    Early monitoring and viz script

---
+++ analyze.py
@@ -0,0 +1,67 @@
+import datetime
+from pprint import pprint
+
+import matplotlib
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.dates import DateFormatter
+
+# First you have to remove first line and convert to UTF-8
+InputFilename = 'submissions.log'
+
+def main():
+    created = {}
+    seen_times = {}
+    seen_ups = {}
+
+    with open(InputFilename, 'r') as f:
+        for line in f:
+            parts = [x for x in line.strip().split("\t")]
+            assert(len(parts) == 4)
+
+            this_time = datetime.datetime.strptime(parts[0].split(".")[0], "%Y-%m-%d %H:%M:%S")
+            # this_time = int(parts[2][0:3])
+            this_id = parts[1]
+            this_created = datetime.datetime.fromtimestamp(float(parts[2]))
+            this_ups = int(parts[3])
+
+            if this_id not in created:
+                created[this_id] = this_created
+                assert(this_id not in seen_times)
+                seen_times[this_id] = []
+                seen_ups[this_id] = []
+
+            assert(created[this_id] == this_created)
+            seen_times[this_id].append(this_time)
+            seen_ups[this_id].append(this_ups)
+
+    print(f"Found {len(created)} unique submissions")
+
+    # fig = plt.figure(1)
+
+    ax = plt.subplot()
+
+    # created = [x for x in created if (datetime.datetime.now() - created[x] < datetime.timedelta(hours=12))]
+
+    print(len(created))
+
+    for id in created:
+        plt.plot_date(seen_times[id], seen_ups[id], xdate=True, ls='solid', lw=0.5, marker=None)
+
+    # ax.grid(color='k', alpha=0.15, which='major')
+
+    formatter = DateFormatter('%H:%M')
+    ax.xaxis.set_major_formatter(formatter)
+
+    # ax.set_ylim([0, 100])
+
+    # plt.legend(list(created))
+    # plt.title('Return factor')
+    # plt.xlabel('new')
+    # plt.ylabel('calculate(new, old)')
+    plt.tight_layout()
+
+    plt.show()
+
+if __name__ == '__main__':
+    main()
---
+++ monitor/src/monitor.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+import logging
+import pprint
+import re
+import time
+
+import praw
+
+def main():
+    print("STARTING")
+
+    reddit = praw.Reddit(
+        client_id=,
+        client_secret=,
+        username=,
+        password=,
+        user_agent=
+    )
+
+    while True:
+        now = datetime.now()
+        for s in reddit.subreddit("memeeconomy").new(limit=None):
+            print(f"{now}\t{s.id}\t{s.created_utc}\t{s.ups}")
+        time.sleep(600)
+
+if __name__ == "__main__":
+    main()
