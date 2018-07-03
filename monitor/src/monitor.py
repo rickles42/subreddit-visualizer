aefd2f40859fdee45825d405344e0ada0e10bd14
rickles42 <81503+rickles42@users.noreply.github.com>
2018-07-03 09:38:43 -0700

    Look at submission.score too

--- monitor/src/monitor.py
+++ monitor/src/monitor.py
@@ -20,7 +20,7 @@ def main():
     while True:
         now = datetime.now()
         for s in reddit.subreddit("memeeconomy").new(limit=None):
-            print(f"{now}\t{s.id}\t{s.created_utc}\t{s.ups}")
+            print(f"{now}\t{s.id}\t{s.created_utc}\t{s.ups}\t{s.score}")
         time.sleep(600)
 
 if __name__ == "__main__":
